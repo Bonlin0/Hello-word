eb74336f50e7be472a7918b89e0c3cf2a8916348
SairenHecate <37873091+SairenHecate@users.noreply.github.com>
2019-11-07 15:57:28 +0800

    为 BaseActivity.java 添加注释

--- Helloword/app/src/main/java/cn/adminzero/helloword/BaseActivity.java
+++ Helloword/app/src/main/java/cn/adminzero/helloword/BaseActivity.java
@@ -2,7 +2,7 @@ package cn.adminzero.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+// 这是一个所有活动的父类
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
