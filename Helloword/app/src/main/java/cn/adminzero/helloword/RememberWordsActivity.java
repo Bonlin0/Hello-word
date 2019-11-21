c0a12f6183f75722b8a84928296c024d7308d8b2
Hell05 <32389641+Bonlin0@users.noreply.github.com>
2019-11-21 19:36:52 +0800

    完成背单词的UI

--- Helloword/app/src/main/java/cn/adminzero/helloword/RememberWordsActivity.java
+++ Helloword/app/src/main/java/cn/adminzero/helloword/RememberWordsActivity.java
@@ -2,7 +2,9 @@ package cn.adminzero.helloword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RememberWordsActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RememberWordsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remember_words);
+    }
+
+    public void rememberButtonOnClicked(View view)
+    {
+        Intent intent = new Intent(RememberWordsActivity.this, ShowWordActivity.class);
+        startActivity(intent);
     }
 }
