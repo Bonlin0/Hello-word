43a1f3970cab97be9a97641378b28109ca60dd72
Hell05 <32389641+Bonlin0@users.noreply.github.com>
2019-11-25 18:33:49 +0800

    更新PK部分UI

--- Helloword/app/src/main/java/cn/adminzero/helloword/GameQueueActivity.java
+++ Helloword/app/src/main/java/cn/adminzero/helloword/GameQueueActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class GameQueueActivity extends AppCompatActivity {
@@ -22,6 +23,15 @@ public class GameQueueActivity extends AppCompatActivity {
                 Intent intent = new Intent(GameQueueActivity.this,WordTestActivity.class);
                 startActivity(intent);
                 // TODO 不知道是否需要在打开新活动后结束自己？
+                finish();
+            }
+        });
+
+        Button exit_game_queue_button = findViewById(R.id.exit_game_queue_button);
+        exit_game_queue_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 退出需要做的工作
                 finish();
             }
         });
